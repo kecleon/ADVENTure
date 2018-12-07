794a0455f819c3a9a90e07f2c8f39a8e3675eb17
kecleon <ultimatedood@gmail.com>
2018-12-06 17:01:40 -0800

    day2 part2

--- ADVENTure/ADVENTure/ADVENTure.cs
+++ ADVENTure/ADVENTure/ADVENTure.cs
@@ -11,7 +11,7 @@ namespace ADVENTure
 	{
 		public static void Main(string[] args)
 		{
-			day1p1();
+			day1p2();
 			Console.ReadLine();
 		}
 
@@ -53,6 +53,59 @@ namespace ADVENTure
 				}
 			}
 			Console.WriteLine("Result: " + runningCount);
+		}
+
+		public static void day1p2()
+		{
+			string[] lines = File.ReadAllLines("day1.txt");
+			int runningCount = 0;
+			List<int> list = new List<int>();
+			bool duplicateFound = false;
+			do
+			{
+				foreach (string line in lines)
+				{
+					if (line.Length == 1) { continue; }
+
+					if (line.StartsWith("+"))
+					{
+						int addAmount = 0;
+						if (int.TryParse(line.Substring(1), out addAmount))
+						{
+							runningCount += addAmount;
+						}
+						else
+						{
+							Console.WriteLine("Failed to parse: " + line);
+						}
+					}
+					else if (line.StartsWith("-"))
+					{
+						int subAmount = 0;
+						if (int.TryParse(line.Substring(1), out subAmount))
+						{
+							runningCount -= subAmount;
+						}
+						else
+						{
+							Console.WriteLine("Failed to parse: " + line);
+						}
+					}
+					else
+					{
+						Console.WriteLine("WTF BOOM:" + line);
+					}
+
+					if (list.Contains(runningCount))
+					{
+						Console.WriteLine($"First repeat: {runningCount},");
+						duplicateFound = true;
+						break;
+					}
+					list.Add(runningCount);
+				}
+				//Console.WriteLine("Result: " + runningCount);
+			} while (!duplicateFound);
 		}
 	}
 }
