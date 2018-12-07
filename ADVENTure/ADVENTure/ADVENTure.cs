99f70aeb0e8949c2a71afe31a02e0d1ce3bcca08
kecleon <ultimatedood@gmail.com>
2018-12-06 20:20:33 -0800

    day2 part1

--- ADVENTure/ADVENTure/ADVENTure.cs
+++ ADVENTure/ADVENTure/ADVENTure.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -106,6 +106,75 @@ namespace ADVENTure
 				}
 				//Console.WriteLine("Result: " + runningCount);
 			} while (!duplicateFound);
+		}
+
+		public static void day2p1()
+		{
+			string[] lines = File.ReadAllLines("input/day2.txt");
+			long checksum = 0;
+
+			List<Dictionary<char, int>> LineCharCountList = new List<Dictionary<char, int>>();
+			foreach (string line in lines)
+			{
+				Dictionary<char, int> CharCount = new Dictionary<char, int>();
+
+				foreach (char c in line)
+				{
+					if (CharCount.ContainsKey(c))
+					{
+						CharCount[c]++;
+					}
+					else
+					{
+						CharCount.Add(c, 1);
+					}
+					//Console.Write(CharCount[c]);
+				}
+
+				//Console.WriteLine("");
+				LineCharCountList.Add(CharCount);
+			}
+
+			Dictionary<int, int> Repeats = new Dictionary<int, int>();
+
+			int twos = 0;
+			int threes = 0;
+
+			foreach (Dictionary<char, int> count in LineCharCountList)
+			{
+				int localtwos = 0;
+				int localthrees = 0;
+
+				foreach (KeyValuePair<char, int> entry in count)
+				{
+					if (entry.Value == 2)
+					{
+						localtwos++;
+					}
+					else if (entry.Value == 3)
+					{
+						localthrees++;
+					}
+					//if (localthrees > 0 && localthrees > 0)
+					//{
+					//	break;
+					//}
+				}
+				//Console.WriteLine($"Twos: {localtwos}, Threes: {localthrees}");
+
+				if (localtwos > 0)
+				{
+					twos++;
+				}
+				if (localthrees > 0)
+				{
+					threes++;
+				}
+			}
+
+			checksum = twos * threes;
+			Console.WriteLine($"ALL Twos: {twos}, Threes: {threes}");
+			Console.WriteLine("Result: " + checksum);
 		}
 	}
 }
